d350021c79dd2d2bbd23b54575f0137dfaf7cd81
aemorozov <mclenina19@gmail.com>
2023-03-17 18:29:37 +0400

    chore: add css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
-import Header from "./Components/Header"
+import Header from "./components/Header"
+import './css/main.css'
 
 class App extends React.Component {
     helpText = 'Help text'
