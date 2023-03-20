83f4a161e57020abf05d7b8be34d24fecbcdbb5e
aemorozov <mclenina19@gmail.com>
2023-03-20 12:33:51 +0400

    chore: clean code, add users, use map and if

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React from "react"
 import Header from "./components/Header"
-import Image from "./components/Image"
-import logo from './img/logo.png'
+import Users from "./components/Users"
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            helpText: "Help text",
-            userData: "",
-        }
-
-        this.inputClick = this.inputClick.bind(this)
-    }
 
     render() {
         return (
             <>
-                <Header title="Header title" />
-                <h1>{this.state.helpText}</h1>
-                <h2>{this.state.userData}</h2>
-                <input placeholder={this.state.helpText}
-                    onChange={event => this.setState({ userData: event.target.value })}
-                    onClick={this.inputClick}
-                    onMouseEnter={this.mouseOver} />
-                <p>{this.state.helpText === 'Help text' ? 'yes' : 'no'}</p>
-                <Image image={logo} />
+                <Header title="Список пользователей" />
+                <main>
+                    <Users />
+                </main>
+                <aside></aside>
             </>
         )
     }
-    inputClick() {
-        this.setState({ helpText: "Changed" })
-        console.log('Clicked')
-    }
-    mouseOver() { console.log('Mouse Over') }
 }
 
 export default App
---
+++ src/Components/Users.js
@@ -0,0 +1,49 @@
+import React from "react"
+
+class Users extends React.Component {
+
+    users = [
+        {
+            id: 1,
+            firstname: 'Bob',
+            lastname: 'Marley',
+            bio: 'Magna sunt deserunt id incididunt occaecat tempor pariatur nulla ullamco labore magna.',
+            age: 40,
+            isHappy: true
+        },
+        {
+            id: 2,
+            firstname: 'John',
+            lastname: 'Doe',
+            bio: 'Esse consequat enim laboris ex exercitation culpa labore Lorem qui occaecat aliqua.',
+            age: 42,
+            isHappy: false
+        }
+    ]
+
+    render() {
+        if (this.users.length > 0) {
+            return (
+                <>
+                    {this.users.map((user) => (
+                        <div key={user.id} className="user" >
+                            <h3>Name: {user.firstname} {user.lastname}</h3>
+                            <p>Bio: {user.bio}</p>
+                            <p style={{ fontWeight: '600' }}>Age: {user.age}</p>
+                            <b>{user.isHappy ? `He is happy` : `He isn't happy`}</b>
+                        </div>
+                    ))}
+                </>
+            )
+        } else {
+            return (
+                <div className="user" >
+                    <h3>Пользователей нет</h3>
+                </div>
+            )
+        }
+
+    }
+}
+
+export default Users
