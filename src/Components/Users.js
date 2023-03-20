0b86338bb0fc94787550873dcc86d78522b90ba1
aemorozov <mclenina19@gmail.com>
2023-03-20 12:49:42 +0400

    chore: add state for users

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -2,30 +2,35 @@ import React from "react"
 
 class Users extends React.Component {
 
-    users = [
-        {
-            id: 1,
-            firstname: 'Bob',
-            lastname: 'Marley',
-            bio: 'Magna sunt deserunt id incididunt occaecat tempor pariatur nulla ullamco labore magna.',
-            age: 40,
-            isHappy: true
-        },
-        {
-            id: 2,
-            firstname: 'John',
-            lastname: 'Doe',
-            bio: 'Esse consequat enim laboris ex exercitation culpa labore Lorem qui occaecat aliqua.',
-            age: 42,
-            isHappy: false
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstname: 'Bob',
+                    lastname: 'Marley',
+                    bio: 'Magna sunt deserunt id incididunt occaecat tempor pariatur nulla ullamco labore magna.',
+                    age: 40,
+                    isHappy: true
+                },
+                {
+                    id: 2,
+                    firstname: 'John',
+                    lastname: 'Doe',
+                    bio: 'Esse consequat enim laboris ex exercitation culpa labore Lorem qui occaecat aliqua.',
+                    age: 42,
+                    isHappy: false
+                }
+            ]
         }
-    ]
+    }
 
     render() {
-        if (this.users.length > 0) {
+        if (this.state.users && this.state.users.length > 0) {
             return (
                 <>
-                    {this.users.map((user) => (
+                    {this.state.users.map((user) => (
                         <div key={user.id} className="user" >
                             <h3>Name: {user.firstname} {user.lastname}</h3>
                             <p>Bio: {user.bio}</p>
